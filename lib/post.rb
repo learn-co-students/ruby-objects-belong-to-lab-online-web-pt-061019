e64c93bc8ee9f0011c06a209a236b39e3f205cf5
layla dantas <laylatdantas@gmail.com>
2019-06-18 19:28:42 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,11 @@
+class Post 
+  attr_accessor :title, :author 
+end 
+
+
+  
+  
+  
+ post = Post.new()
+ post.title  = "Hello World"
+ post.author = "Uncle Bob"
---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+class Song 
+  attr_accessor :title, :artist
+end 
+
+
+  
+  
+  
+ song = Song.new()
+ song.title  = "7/11"
+ song.artist = "Beyonce"
+
+
+
+
